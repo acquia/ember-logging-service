575deff4804832b65f923c948d3e0838cfef2402
Lisa Backer <lisa.backer@acquia.com>
2016-12-07 12:49:12 -0500

    Added error monitoring into core logging service

--- app/instance-initializers/ember-logging-service.js
+++ app/instance-initializers/ember-logging-service.js
@@ -1,8 +1,10 @@
 import ENV from '../config/environment';
 import setupLoggingService from 'ember-logging-service/instance-initializers/setup-logging-service';
+import setupErrorMonitoring from 'ember-logging-service/instance-initializers/setup-error-monitoring';
 
 export function initialize(instance) {
   setupLoggingService(instance, ENV);
+  setupErrorMonitoring(instance, ENV);
 }
 
 export default {
